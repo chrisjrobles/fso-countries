7ab394fe7735a7226efe78233de88fe5d963ad87
Christopher Robles <crobles@nerdwallet.com>
2022-11-22 20:42:00 -0800

    feat: fso-countries init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const getResults = (matches, countryData) => {
+  if (!matches) {
+    return;
+  }
+  if (matches.length === 1) {
+    const country = countryData[matches[0]];
+    return (
+      <div>
+        <h2>{country.name.common}</h2>
+        <p style={{ margin: 0 }}>capital {country.capital[0]}</p>
+        <p style={{ margin: 0 }}>area {country.area}</p>
+        <h3>languages:</h3>
+        <ul>
+          {Object.values(country.languages).map((language) => (
+            <li key={language}>{language}</li>
+          ))}
+        </ul>
+        <img
+          src={country.flags.svg}
+          alt={`${country.name.common} flag`}
+          width={200}
+        />
+      </div>
+    );
+  }
+  if (matches.length > 10) {
+    return <p>Too many matches, specify another filter</p>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ul style={{ listStyle: 'none', padding: 0 }}>
+      {matches.map((match) => (
+        <li key={match}>{match}</li>
+      ))}
+    </ul>
+  );
+};
+
+const App = () => {
+  const [matches, setMatches] = useState([]);
+  const [countryData, setCountryData] = useState({});
+  const [countryNames, setCountryNames] = useState([]);
+
+  const onChange = (e) => {
+    const query = e.target.value;
+    setMatches(
+      countryNames.filter((countryName) =>
+        countryName.toLowerCase().includes(query)
+      )
+    );
+  };
+
+  useEffect(() => {
+    const getCountryData = async () => {
+      const response = await axios.get('https://restcountries.com/v3.1/all');
+      const countries = response.data;
+      setCountryNames(countries.map((country) => country.name.common));
+      setCountryData(
+        countries.reduce(
+          (total, prev) => ({ ...total, [prev.name.common]: prev }),
+          {}
+        )
+      );
+    };
+    getCountryData();
+  }, []);
+
+  return (
+    <div>
+      find countries <input type='text' onChange={onChange}></input>
+      {getResults(matches, countryData)}
     </div>
   );
-}
+};
 
 export default App;
